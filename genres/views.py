2063613d7faa67aee022ab77624503949bf55b25
Ramon Leopoldo <ramon.jl@outlook.com>
2024-11-22 09:34:18 -0300

    Criação do metodo post da API de generos

--- genres/views.py
+++ genres/views.py
@@ -1,7 +1,17 @@
+import json
+from django.views.decorators.csrf import csrf_exempt
 from genres.models import GenresModel
 from django.http import JsonResponse
 
+@csrf_exempt
 def genres_list_view(request):
-    genres = GenresModel.objects.all()
-    data = [{'id':genre.id,'name':genre.name}for genre in genres]
-    return JsonResponse(data,safe=False)
+    if request.method == 'GET':
+        genres = GenresModel.objects.all()
+        data = [{'id':genre.id,'name':genre.name}for genre in genres]
+        return JsonResponse(data,safe=False)
+    elif request.method == 'POST':
+        data = json.loads(request.body.decode('utf-8'))
+        new_genre = GenresModel(name = data['name'])
+        new_genre.save()
+        return JsonResponse({'id':new_genre.id , 'name':new_genre.name}, status=201 )
+        
