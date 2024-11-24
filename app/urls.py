9a7e6401e91324468c8bbd36b6643ee2760da8a9
Ramon Leopoldo <ramon.jl@outlook.com>
2024-11-24 16:31:01 -0300

    Criação de genre_details_view que realiza ações de detalhes de objetos, atualização de objetos e exclusão.

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path
-from genres.views import genres_list_view
+from genres.views import genre_created_list_view,genre_detail_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('genres/', genres_list_view, name= 'genres')
+    path('genres/', genre_created_list_view, name= 'genres_created_list'),
+    path('genres/<int:pk>/', genre_detail_view, name='genre_detail'),
 ]
--- genres/views.py
+++ genres/views.py
@@ -2,9 +2,10 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 from genres.models import GenresModel
 from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 
 @csrf_exempt
-def genres_list_view(request):
+def genre_created_list_view(request):
     if request.method == 'GET':
         genres = GenresModel.objects.all()
         data = [{'id':genre.id,'name':genre.name}for genre in genres]
@@ -14,4 +15,18 @@ def genres_list_view(request):
         new_genre = GenresModel(name = data['name'])
         new_genre.save()
         return JsonResponse({'id':new_genre.id , 'name':new_genre.name}, status=201 )
+@csrf_exempt  
+def genre_detail_view(request, pk):
+    genre = get_object_or_404(GenresModel,pk=pk)
+    if request.method == 'GET':
+        data = {'id':genre.id, 'name':genre.name}
+        return JsonResponse(data)
+    elif request.method == 'PUT' :
+        data = json.loads(request.body.decode('utf-8'))
+        genre.name = data['name']
+        genre.save()
+        return JsonResponse({'id':genre.id , 'name':genre.name})
+    elif request.method == 'DELETE':
+        genre.delete()
+        return JsonResponse({'id':genre.id, 'name':genre.name, 'status':'excluido'}, status = 204)
         
