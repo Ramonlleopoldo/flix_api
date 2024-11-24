86694755b629ba2fbc77918bb37bcb84260b9bb1
Ramon Leopoldo <ramon.jl@outlook.com>
2024-11-24 19:49:32 -0300

    Modificando as function based views de genre para class based views utilizando o rest based views, criação do arquivo serializers.

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path
-from genres.views import genre_created_list_view,genre_detail_view
+from genres.views import GenreListCreateView,GenreRetriveUpdateDelete
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('genres/', genre_created_list_view, name= 'genres_created_list'),
-    path('genres/<int:pk>/', genre_detail_view, name='genre_detail'),
+    path('genres/', GenreListCreateView.as_view(), name= 'genres_created_list'),
+    path('genres/<int:pk>/',GenreRetriveUpdateDelete.as_view(), name='genre_detail'),
 ]
--- genres/views.py
+++ genres/views.py
@@ -1,32 +1,11 @@
-import json
-from django.views.decorators.csrf import csrf_exempt
+from rest_framework import generics
 from genres.models import GenresModel
-from django.http import JsonResponse
-from django.shortcuts import get_object_or_404
+from genres.serializers import GenreSerializer
 
-@csrf_exempt
-def genre_created_list_view(request):
-    if request.method == 'GET':
-        genres = GenresModel.objects.all()
-        data = [{'id':genre.id,'name':genre.name}for genre in genres]
-        return JsonResponse(data,safe=False)
-    elif request.method == 'POST':
-        data = json.loads(request.body.decode('utf-8'))
-        new_genre = GenresModel(name = data['name'])
-        new_genre.save()
-        return JsonResponse({'id':new_genre.id , 'name':new_genre.name}, status=201 )
-@csrf_exempt  
-def genre_detail_view(request, pk):
-    genre = get_object_or_404(GenresModel,pk=pk)
-    if request.method == 'GET':
-        data = {'id':genre.id, 'name':genre.name}
-        return JsonResponse(data)
-    elif request.method == 'PUT' :
-        data = json.loads(request.body.decode('utf-8'))
-        genre.name = data['name']
-        genre.save()
-        return JsonResponse({'id':genre.id , 'name':genre.name})
-    elif request.method == 'DELETE':
-        genre.delete()
-        return JsonResponse({'id':genre.id, 'name':genre.name, 'status':'excluido'}, status = 204)
-        
+class GenreListCreateView(generics.ListCreateAPIView):
+    queryset = GenresModel.objects.all()
+    serializer_class = GenreSerializer
+
+class GenreRetriveUpdateDelete(generics.RetrieveUpdateDestroyAPIView):
+    queryset = GenresModel.objects.all()
+    serializer_class = GenreSerializer
